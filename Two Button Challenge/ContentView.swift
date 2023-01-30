a16afc063bf556cec565454ae8cfcd48eda4a31c
anthonymarie22 <72616953+anthonymarie22@users.noreply.github.com>
2023-01-30 17:07:19 -0500

    button challenge 2

--- Two Button Challenge/ContentView.swift
+++ Two Button Challenge/ContentView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = ""
+    @State private var imageString = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           
+            Spacer()
+            
+            Image(systemName: imageString)
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(Color(.gray))
+                .padding()
+            
+            Spacer()
+            Text(messageString)
+                .foregroundColor(.red)
+                .fontWeight(.black)
+                .font(.largeTitle)
+                .padding()
+            
+         
+            
+            HStack{
+                Button("Swiftly") {
+                    messageString = "You Swiftly!"
+                    imageString = "swift"
+                                    
+                }
+                Button("Apps!") {
+                    messageString = "You Build Apps"
+                    imageString = "apps.iphone"
+                    
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
     }
